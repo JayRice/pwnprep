501c87b745b2b6c457434ffa4bf33c66869a864a
JayRice <jayjrice1@gmail.com>
2025-07-01 18:40:22 -0500

    Last commit before deployment 6/30 Final

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -1,4 +1,5 @@
-import {useRef} from "react"
+import { useRef, useState} from "react"
+import LoadingSpinner from "./LoadingSpinner"
 
 interface Props{
     confirming_reason: string;
@@ -11,6 +12,10 @@ export default function ConfirmationModal({confirming_reason, confirm_callback, 
 
     const confirmationModalRef = useRef<HTMLDivElement>(null);
 
+    const [isLoading, setIsLoading] = useState(false);
+    // useEffect(() => {
+    //
+    // }, []);
     return (
         <div className="fixed inset-0 z-[1000] bg-black bg-opacity-50 flex items-center justify-center p-4 w-full h-full">
             <div ref={confirmationModalRef}  className="bg-white rounded-lg shadow-xl w-full max-w-md h-[60vh] flex flex-col p-16 items-center">
@@ -19,7 +24,10 @@ export default function ConfirmationModal({confirming_reason, confirm_callback, 
 
                 <p className={"text-md relative top-8 text-center"}>{description}</p>
                 <div className={"absolute flex flex-row w-full  top-[70%]  gap-4  justify-center"}>
-                    <button onClick={confirm_callback} className={"flex items-center justify-center   h-12 text-white font-bold bg-red-600 hover:bg-red-800 p-4 transition-colors"}>Confirm</button>
+                    <button disabled={isLoading} onClick={() => {
+                        setIsLoading(true)
+                        confirm_callback()
+                    }} className={"flex items-center justify-center   h-12 text-white font-bold bg-red-600 hover:bg-red-800 p-4 transition-colors"}>{ isLoading ? <LoadingSpinner />:"Confirm"}</button>
                     <button onClick={cancel_callback} className={"flex items-center justify-center  h-12 border-2 border-black hover:bg-gray-800 hover:text-white p-4 transition-colors "}>Cancel</button>
 
                 </div>
--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -30,7 +30,7 @@ export default function ProfilePage({user} : Props) {
     * providerData : [{"providerId":"google.com","uid":"102870588615729799220","displayName":"Jayden R","email":"jayjrice1@gmail.com","phoneNumber":null,"photoURL":"https://lh3.googleusercontent.com/a/ACg8ocKJYNVzZnwVkGp4B8AhBv71gwN46WShy5BftH7Lq2KCaCHDyg=s96-c"}]
     * */
     return (
-        <div className={"flex justify-center w-full h-full"}>
+        <div className={"flex justify-center w-full h-full break-words"}>
             <Toaster position={"bottom-right"}></Toaster>
 
             {isDeletingUser &&
@@ -62,30 +62,31 @@ export default function ProfilePage({user} : Props) {
 
             }}/>}
 
-            <div className=" w-[40vw] h-[70vh] rounded-lg bg-purple-600 mt-12 text-white p-4">
+            <div className=" w-[40vw] h-full min-h-[70vh] rounded-lg border-4 border-gray-600 text-gray-600 mt-12 text-black p-4">
 
                 <h1 className={"font-bold text-3xl mt-4 text-center mb-6"}> Profile </h1>
 
-                <p className="text-white-600 text-2xl mb-6"> User Id: {user.uid}</p>
+                <p className=" text-2xl mb-6"> User Id: {user.uid}</p>
 
-                <p className="text-white-600 text-2xl mb-6"> Email: {user.email}</p>
+                <p className="text-2xl mb-6"> Email: {user.email}</p>
 
-                <p className="text-white-600 text-2xl mb-6"> Premium : {premium ? "Active":"Inactive"} </p>
+                <p className="text-2xl mb-6"> Premium : {premium ? "Active":"Inactive"} </p>
 
 
 
-                <div className={"flex flex-row gap-4"}>
+                <div className={"flex flex-row justify-center items-center gap-4 bottom-2 mt-12"}>
 
 
 
                     {premium && <button onClick={() => {
                         setIsCancelingSubscription(true)
-                    }} className=" font-bold text-2xl mb-6  text-red-600 underline hover:text-red-800 transition-colors"> Cancel Subscription  </button>}
+                    }} className=" font-bold text-2xl   text-red-600 underline hover:text-red-800 transition-colors"> Cancel Subscription </button>}
+
+                    <button onClick={() => {
+                        setIsDeletingUser(true);
+                    }} className="font-bold text-2xl  text-red-600 underline hover:text-red-800 transition-colors"> Delete Account </button>
 
                 </div>
-                <button onClick={() => {
-                    setIsDeletingUser(true);
-                }} className="font-bold text-2xl mt-14 text-red-600 underline hover:text-red-800 transition-colors"> Delete Account </button>
 
 
 
